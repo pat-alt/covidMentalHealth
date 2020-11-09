8ad07a0243a793de023f43043febff9a2e80b337
marcagusti <marc.agusti@barcelonagse.eu>
2020-11-09 15:56:19 +0100

    Ahora esta mejor hermanos

--- covid_API.py
+++ covid_API.py
@@ -1,6 +1,16 @@
 import requests
+import pandas as pd
+
+# Here we can see the latest status
 res = requests.get('https://covid19-api.org/api/status')
 res.json()
-import pandas as pd
 df = pd.DataFrame(res.json())
 df['country']
+df
+
+# Here we can see the diff between the latest state and previuos one
+
+diff = requests.get('https://covid19-api.org/api/diff')
+diff.json()
+diff = pd.DataFrame(diff.json())
+diff
