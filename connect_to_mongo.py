d4db14722aadd9a1d0c707898867fd7c006620af
Polina Berenstein <polinaberen@gmail.com>
2020-11-10 19:46:16 +0100

    comments with basic mongo functinos

--- connect_to_mongo.py
+++ connect_to_mongo.py
@@ -1,11 +1,25 @@
 from pymongo import MongoClient
 from pprint import pprint
 
-
-# connect to MongoDB, change the << MONGODB URL >> to reflect your own connection string
 client = MongoClient('mongodb://3.22.27.22:27017')
 
-db=client.admin
-# Issue the serverStatus command and print the results
-serverStatusResult=db.command("serverStatus")
-pprint(serverStatusResult)
+#switch to test db
+db = client.test
+
+#insert data to inventory schema
+db.inventory.insert_one(
+    {"item": "canvas",
+     "qty": 100,
+     "tags": ["cotton"],
+     "size": {"h": 28, "w": 35.5, "uom": "cm"}})
+
+# load inventory schema into cursor
+cursor = db.inventory.find({})
+
+#print all objects in inventory
+for inventory in cursor:
+     pprint(inventory)
+
+#delete all canvas objects in inventory
+db.inventory.delete_many({"item": "canvas"})
+
