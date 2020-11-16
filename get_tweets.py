967ee0e69a94c2a919216e87c0c4c4ec5c243271
pat-alt <altmeyerpat@gmail.com>
2020-11-16 09:51:39 +0100

    set up listener but cannot authenticate

--- get_tweets.py
+++ get_tweets.py
@@ -1,5 +1,4 @@
 ## Script to get tweet data
-
 from config import * # configuration
 import tweepy as twpy
 
@@ -7,6 +6,26 @@ import tweepy as twpy
 auth = twpy.OAuthHandler(twitter_app_key, twitter_app_secret)
 auth.set_access_token(twitter_key, twitter_secret)
 # Create API object:
-api = twpy.API(auth)
+api = twpy.API(auth, parser=twpy.parsers.JSONParser())
 
+# Search terms:
+mental_health = ["mentalhealth","mental","health","suicide","depression"]
+
+## SEARCHING ----
+results = api.search(q=mental_health, count=10)
+
+## STREAMING -----
 # Set up stream listener
+class StreamListener(twpy.StreamListener):
+    def on_status(self, status):
+        if status.retweeted_status:
+            return
+        return status
+    def on_error(self, status_code):
+        if status_code == 420:
+            return False
+
+# Listen
+stream_listener = StreamListener()
+stream = twpy.Stream(auth=api.auth, listener=stream_listener)
+# stream.filter(track=mental_health)
