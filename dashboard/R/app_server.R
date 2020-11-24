7a8e4a55b7046ef7e6e1615a4ad94539bc79de23
pat-alt <altmeyerpat@gmail.com>
2020-11-24 16:17:53 +0100

    skeleton

---
+++ dashboard/R/app_server.R
@@ -0,0 +1,11 @@
+#' The application server-side
+#'
+#' @param input,output,session Internal parameters for {shiny}.
+#'     DO NOT REMOVE.
+#' @import shiny
+#' @noRd
+app_server <- function( input, output, session ) {
+  # List the first level callModules here
+  callModule(mod_at_a_glance_server, "at_a_glance")
+
+}
---
+++ dashboard/tests/testthat.R
@@ -0,0 +1,4 @@
+library(testthat)
+library(covid_mental_health)
+
+test_check("covid_mental_health")
