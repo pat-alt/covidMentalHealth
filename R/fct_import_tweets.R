9ddf8404d564dac79da90eaf0b48d1e67d01bc3b
pat-alt <altmeyerpat@gmail.com>
2020-11-26 16:41:10 +0100

    first chart

--- R/fct_import_tweets.R
+++ R/fct_import_tweets.R
@@ -1,3 +1,12 @@
+#' Title
+#'
+#' @param n
+#'
+#' @importFrom data.table :=
+#'
+#' @return
+#'
+#' @examples
 import_tweets <- function(n) {
   # Load Python module:
   reticulate::source_python(file.path(Sys.getenv("TRANSFORM_DIR"),"tweets_to_df.py"))
