6d2819bde8c24fcb2bb2b01e695c03b8da8a11e9
pat-alt <altmeyerpat@gmail.com>
2020-11-26 16:03:55 +0100

    some progress on dashboard

--- R/fct_import_covid.R
+++ R/fct_import_covid.R
@@ -1,3 +1,6 @@
-import_covid <- function() {
-
+import_covid <- function(n) {
+  # Load Python module:
+  reticulate::source_python(file.path(Sys.getenv("TRANSFORM_DIR"),"covid_to_df.py"))
+  dt <- data.table::data.table(covid_from_mongo(n)) # executes python function and imports output to R
+  return(dt)
 }
--- R/fct_import_tweets.R
+++ R/fct_import_tweets.R
@@ -1,6 +1,6 @@
 import_tweets <- function(n) {
   # Load Python module:
-  reticulate::source_python(file.path(Sys.getenv("TRANSFORM_DIR"),"transform.py"))
-  dt <- data.table::data.table(load_from_mongo(n)) # executes python function and imports output to R
+  reticulate::source_python(file.path(Sys.getenv("TRANSFORM_DIR"),"tweets_to_df.py"))
+  dt <- data.table::data.table(tweets_from_mongo(n)) # executes python function and imports output to R
   return(dt)
 }
